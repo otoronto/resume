b7c42d5a5618b6df17d00fd5e2bebdecaea03518
Emre Başar <emrebasar1994@gmail.com>
2023-01-17 20:39:19 +0300

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
+import Home from './page/Home.js'
+import Resume from './page/Resume';
+import { BrowserRouter as Router, Route, Redirect, Switch, Routes } from 'react-router-dom';
+import Contact from './page/Contact';
+import Blog from './page/Blog';
+import Portfolio from './page/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <Router>
+        <Routes>
+          <Route exact path ='/' element ={<Home/>}></Route>
+          <Route exact path ='/resume' element ={<Resume/>}></Route>
+          <Route exact path ='/portfolio' element ={<Portfolio/>}></Route>
+          <Route exact path ='/blog' element ={<Blog/>}></Route>
+          <Route exact path ='/contact' element ={<Contact/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
